740ca62c567d1bf5033390b979763abef4ca84c1
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2022-08-13 09:26:24 +0500

    Generic interface for multi sensor data creation.

---
+++ input_components/ground_truth/__init__.py
@@ -0,0 +1,9 @@
+from ..sensor_data import GroundTruth
+
+# Number of sensors
+NUM_SENSORS = 1
+
+# Number of readings per sensor
+NUM_READINGS = 100
+
+readings = GroundTruth(NUM_SENSORS, NUM_READINGS)
